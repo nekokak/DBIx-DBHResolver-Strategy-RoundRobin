9d9539e33c2ef934b98f35bdcfe384d3575f0017
Atsushi Kobayashi <nekokak@gmail.com>
2010-08-23 11:01:21 +0900

    version++

--- lib/DBIx/DBHResolver/Strategy/RoundRobin.pm
+++ lib/DBIx/DBHResolver/Strategy/RoundRobin.pm
@@ -1,7 +1,7 @@
 package DBIx::DBHResolver::Strategy::RoundRobin;
 use strict;
 use warnings;
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 sub connect_info {
     my ($class, $resolver, $label, ) = @_;
